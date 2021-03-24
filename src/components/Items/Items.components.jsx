6124ff1178ec0ff39d93223df59b03900ab28865
chaithrakm1998 <76141182+chaithrakm1998@users.noreply.github.com>
2021-03-24 15:33:55 +0530

    ghg
    ggg

--- src/components/Items/Items.components.jsx
+++ src/components/Items/Items.components.jsx
@@ -3,12 +3,13 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Card} from 'react-bootstrap'
+import './items.styles.scss';
 import Header from '../header/header.components'
 const Items = ({id,name,gender,state,country,city,email,phone,age,occupation,father,mother,imageUrl }) => {
   
   return (
     <div className='user-details'>
-     <div className='header'><Header/></div>
+     <div ><Header/></div>
      
      <br/>
      
@@ -19,8 +20,9 @@ const Items = ({id,name,gender,state,country,city,email,phone,age,occupation,fat
         }}
       />
       <br/>
-      <div className='collection-footer'>
       <h1 className='name'>{name}</h1>
+      <div className='collection-footer'>
+     
         <h3 className='name'>Gender: {gender}</h3>
         <h3 className='name'>Age: {age}</h3>
         <h3 className='name'>City:{city}</h3>
